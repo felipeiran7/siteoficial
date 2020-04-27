555edb9eaf1f2e7f42b21bc295d2377a66201215
Felipe Iran <felipeiran4@gmail.com>
2020-04-27 12:44:25 -0300

    criando a classe page

--- index.php
+++ index.php
@@ -4,17 +4,16 @@
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
+use \Evolucao\Page;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    $sql = new Evolucao\DB\Sql();
-    $results= $sql->select("SELECT *FROM tb_admin");
 
-    echo json_encode($results);
-
+	$page= new Page();
+	$page->setTpl("index");
 });
 
 
