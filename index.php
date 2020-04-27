ea5540a97d8c24a37ac4ea9a8c5090be3563c31f
Felipe Iran <felipeiran4@gmail.com>
2020-04-27 12:10:40 -0300

    configurando banco sql

--- index.php
+++ index.php
@@ -10,7 +10,10 @@ $app = new Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
+    $sql = new Evolucao\DB\Sql();
+    $results= $sql->select("SELECT *FROM tb_admin");
+
+    echo json_encode($results);
 
 });
 
